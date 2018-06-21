95c6d43c986f54bc0faae0b09899c27380dd35c5
huangdongkui <27381011@qq.com>
2018-06-22 00:16:47 +0800

    完成导出功能

--- src/main/java/com/jflyfox/modules/admin/activitymanager/ActivityProjectController.java
+++ src/main/java/com/jflyfox/modules/admin/activitymanager/ActivityProjectController.java
@@ -1,5 +1,6 @@
 package com.jflyfox.modules.admin.activitymanager;
 
+import cn.afterturn.easypoi.excel.entity.params.ExcelExportEntity;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
@@ -12,9 +13,13 @@ import com.jflyfox.system.department.DepartmentSvc;
 import com.jflyfox.system.department.SysDepartment;
 import com.jflyfox.system.user.SysUser;
 import com.jflyfox.util.StrUtils;
+import com.jflyfox.util.easypoi.ExcelExportUtils;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 〈评委评分项目列表〉
@@ -146,7 +151,7 @@ public class ActivityProjectController extends BaseProjectController {
     /**
      * 导出excel
      */
-    private void exportExcel(){
+    public void exportExcel(){
         String departid=getPara("departid");
         if(departid==null){
             departid="-1";
@@ -171,9 +176,44 @@ public class ActivityProjectController extends BaseProjectController {
             sql.append(" order by t.").append(orderBy);
         }
 
-//        Page<BusiActivity> page = BusiActivity.dao.paginate(getPaginator(), "select t.id, t.create_time,activity_name,u.tel,u.realname,d.name as departname,t.project_name,t.from_belongfields ",
-//                sql.toString().toString());
-        final List<BusiActivity> byWhere = BusiActivity.dao.findByWhere(sql.toString());
+        String selectFields="select t.id, t.create_time,activity_name,u.tel,u.realname,d.name as departname," +
+                "t.project_name, (select group_concat(s.detail_name) from sys_dict_detail s\n" +
+                " where s.dict_type='belongfield' \n" +
+                " and FIND_IN_SET(s.detail_code,t.from_belongfields)) as from_belongfields ";
+
+        final List<BusiActivity> byWhere = BusiActivity.dao.find(selectFields+sql.toString());
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        List<ExcelExportEntity> beanList = new ArrayList<ExcelExportEntity>();
+
+        beanList.add(new ExcelExportEntity("序号", "no"));
+        beanList.add(new ExcelExportEntity("项目名称", "project_name"));
+        beanList .add(new ExcelExportEntity("单位", "departname"));
+        beanList .add(new ExcelExportEntity("领域", "from_belongfields"));
+        beanList .add(new ExcelExportEntity("申请人", "realname"));
+        beanList .add(new ExcelExportEntity("联系方式", "tel"));
+        beanList .add(new ExcelExportEntity("负责人", "project_leader"));
+        beanList .add(new ExcelExportEntity("分数", "score"));
+        beanList .add(new ExcelExportEntity("备注", "remarks"));
+
+        // }
+
+        int i=1;
+        for (BusiActivity busiActivity : byWhere) {
+            Map<String, Object> map=new HashMap<>();
+            map.put("no",i++);
+            map.put("project_name",busiActivity.get("project_name"));
+            map.put("departname",busiActivity.get("departname"));
+            map.put("from_belongfields",busiActivity.get("from_belongfields"));
+            map.put("realname",busiActivity.get("realname"));
+            map.put("tel",busiActivity.get("tel"));
+            map.put("project_leader",busiActivity.get("project_leader"));
+            map.put("score",0);
+            map.put("remarks",busiActivity.get("remarks"));
+            list.add(map);
+        }
+
+        renderText(ExcelExportUtils.export(beanList, list));
     }
 
 
--- src/main/java/com/jflyfox/util/easypoi/ExcelExportUtils.java
+++ src/main/java/com/jflyfox/util/easypoi/ExcelExportUtils.java
@@ -3,13 +3,12 @@ package com.jflyfox.util.easypoi;
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.params.ExcelExportEntity;
+import com.jflyfox.system.file.util.FileUploadUtils;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,8 +27,9 @@ public class ExcelExportUtils {
      * @return 路径
      */
     public static String export(List<ExcelExportEntity> column, List<Map<String, Object>> data){
-        String path="D:/excel/ExcelExportForMap.xls";
 
+        String fileName=FileUploadUtils.getBasePath()+"TempExpotFile\\统计报表.xls";
+        String path  = FileUploadUtils.getRootPath()+fileName;
         try {
             Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams(),column,data);
 
@@ -42,6 +42,7 @@ public class ExcelExportUtils {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return path;
+
+        return fileName;
     }
 }
